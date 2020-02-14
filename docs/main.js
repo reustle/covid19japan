add2b7ce9383433b1275136355cdd5f4a0626349
Shane Reustle <shane@reustle.co>
2020-02-15 03:15:51 +0900

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,13 @@
-console.log('hello')
+'use strict';
+mapboxgl.accessToken = 'pk.eyJ1IjoicmV1c3RsZSIsImEiOiJjazZtaHE4ZnkwMG9iM3BxYnFmaDgxbzQ0In0.nOiHGcSCRNa9MD9WxLIm7g';
+
+// Init Map
+var map = new mapboxgl.Map({
+    container: 'map-container',
+    style: 'mapbox://styles/mapbox/light-v10',
+    zoom: 5,
+    center: {
+        lng: 139.75917031249946,
+        lat: 35.6723664164637
+    }
+})
