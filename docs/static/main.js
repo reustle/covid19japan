3de7c79374fde3784a87b105693a5a161000cd43
Shane Reustle <shane@reustle.co>
2020-02-16 10:40:36 +0000

    real data from sheets

--- docs/static/main.js
+++ docs/static/main.js
@@ -35,24 +35,64 @@ map.once('style.load', function(e) {
     buffer: 0,
     //maxzoom: 12
   })
+
+  // Start the Mapbox search expression
+  let prefecturePaint = [
+    'match',
+    ['get', 'NAME_1'],
+  ]
   
-  map.addLayer({
-    'id': 'prefecture-layer',
-    'type': 'fill',
-    'source': 'prefectures',
-    'layout': {},
-    'paint': {
-      'fill-color': [
-        'match',
-        ['get', 'NAME_1'],
-        'Tokyo', '#ca0000',
-        'Kanagawa', '#db6161',
-        'Aichi', '#db6161',
-        'Saitama', '#db6161',
-        '#d0d0d0'
-      ],
-      'fill-opacity': 0.6,
-      'fill-outline-color': '#fff',
-    }
-  })
+  // Load the google spreadsheet
+  async function loadSheet() {
+    
+    // Init the load with drive-db
+    const db = await drive({
+      sheet: '1jfB4muWkzKTR0daklmf8D5F0Uf_IYAgcx_-Ij9McClQ',
+      tab: '2',
+      cache: 3600,
+      onload: data => data
+    })
+    
+    // Go through all prefectures looking for cases
+    db.forEach(function(prefecture){
+      
+      let cases = parseInt(prefecture.cases)
+      if(cases > 0){
+        prefecturePaint.push(prefecture.prefecture)
+        
+        if(cases == 1){
+          // 1 case
+          prefecturePaint.push('#fceacf')
+        }else if(cases <= 5){
+          // 2-5 cases
+          prefecturePaint.push('#f89a85')
+        }else if(cases <= 10){
+          // 6-10 cases
+          prefecturePaint.push('#e55c4e')
+        }else{
+          // 11+ cases
+          prefecturePaint.push('#cb2c38')
+        }
+      }
+      
+    })
+    
+    // Add the final value to use as the default color
+    prefecturePaint.push('#d0d0d0')
+    console.log(prefecturePaint)
+    
+    map.addLayer({
+      'id': 'prefecture-layer',
+      'type': 'fill',
+      'source': 'prefectures',
+      'layout': {},
+      'paint': {
+        'fill-color': prefecturePaint,
+        'fill-opacity': 0.75,
+        'fill-outline-color': '#fff',
+      }
+    })
+  }
+  loadSheet()
+  
 })
