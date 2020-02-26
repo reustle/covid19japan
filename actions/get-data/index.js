a9379c8d9cc1d0cd339258f8d7ac0a38ca1bdc48
Nick Olinger <olingern@gmail.com>
2020-02-26 23:34:44 +0900

    ignore node_modules
    file ext

--- actions/get-data/index.js
+++ actions/get-data/index.js
@@ -1,3 +1,3 @@
 console.log("Running action ...")
-require("../../scripts/cache-spreadsheet-data/cache-sheet")
+require("../../scripts/cache-spreadsheet-data/cache-sheet.js")
 console.log("Action finish")
