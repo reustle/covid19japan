e1a1d861501b7ca83a4702e5798ef30180e78564
Alastair Tse <alastair@liquidx.net>
2020-03-19 22:07:53 +0900

    Enable babel on webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -49,6 +49,15 @@ module.exports = {
         ]
       },
       {
+        test: /\.m?js$/, exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+      },
+      {
         test: /\.scss$/,
         use: [
           { loader: MiniCssExtractPlugin.loader },
