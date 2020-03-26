fac8f9277ad7717ed6aa1ae1ee336d5221e86c35
Ann Kilzer <ann@curvegrid.com>
2020-03-26 20:50:00 +0900

    small fixes

--- src/index.js
+++ src/index.js
@@ -404,21 +404,6 @@ function drawForeignBordersTable(countries) {
   })
 }
 
-function drawForeignBordersTable(countries) {
-  let dataTable = document.querySelector('#foreign-borders-table tbody')
-
-  // Remove the loading cell
-  dataTable.innerHTML = ''
-
-  // Iterate through and render table rows
-  _.orderBy(countries, 'name', 'desc').map(function(country){
-    let name = (LANG == 'en') ? country.name : country.nameJa
-
-    dataTable.innerHTML = dataTable.innerHTML + "<tr><td>" + name + "</td><td>" + (country.noEntry ? "X" : "") + "</td><td>" + country.details + "</td></tr>"
-    return true
-  })
-}
-
 function drawKpis(totals, totalsDiff) {
   // Draw the KPI values
 
