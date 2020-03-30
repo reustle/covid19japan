c2ff74c95749fbf4705f9d3cee964ea00140007d
mertd <>
2020-03-30 09:49:43 +0900

    fix after solving merge conflicts

--- src/index.js
+++ src/index.js
@@ -447,7 +447,6 @@ function drawTrendChart(sheetTrend) {
         tick: {
           values: [0, 100, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000]
         }
-      }
     },
     tooltip: {
       format: {
