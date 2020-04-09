904eb066671b8a487170defdf727fd3a89f28b09
Shane Reustle <shane@reustle.co>
2020-04-10 02:19:20 +0900

    Update DailyIncrease.js

--- src/components/DailyIncreaseChart/DailyIncrease.js
+++ src/components/DailyIncreaseChart/DailyIncrease.js
@@ -76,7 +76,16 @@ const drawDailyIncreaseChart = (sheetTrend, dailyIncreaseChart) => {
       y: {
         tick: {
           values: [
-            1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000
+            100,
+            200,
+            300, 
+            400, 
+            500,
+            600, 
+            700,
+            800,
+            900,
+            1000
           ],
         },
       },
