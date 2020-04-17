4dfe001c9d28143b8f61ee16d0b6104060b5ddec
Mert Demir <mertd@users.noreply.github.com>
2020-04-17 19:19:00 +0900

    use const for variables that are not reassigned
    Co-Authored-By: Alexander Ogilvie <26034704+a-ogilvie@users.noreply.github.com>

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,8 +1,8 @@
 export const LANGUAGES = ["en", "ja"];
 
 const generateExport = () => {
-  let resources = {};
-  for (let language of LANGUAGES) {
+  const resources = {};
+  for (const language of LANGUAGES) {
     resources[language] = {
       translation: require(`./${language}.json`),
     };
