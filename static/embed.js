60e5c6cf531c8748fd837480727609940cc82c2e
mertd <mertd@users.noreply.github.com>
2020-04-25 18:01:05 +0900

    add very basic embed

--- static/embed.js
+++ static/embed.js
@@ -1,3 +1,6 @@
 /**
  * Embed covid19japan.com into your web app
  */
+
+const element = document.getElementById("covid19japan-embed");
+element.innerHTML = `<iframe src="https://covid19japan.com/embed"/>`;
