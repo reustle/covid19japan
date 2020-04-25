0f5b0ab8d9097c9dbc584ae08a91e8cec982e5ba
mertd <mertd@users.noreply.github.com>
2020-04-25 18:17:33 +0900

    add basic style

--- static/embed.js
+++ static/embed.js
@@ -3,4 +3,10 @@
  */
 
 const element = document.getElementById("covid19japan-embed");
-element.innerHTML = `<iframe src="https://covid19japan.com/embed"/>`;
+const style = `
+    border: 0;
+    width: 100%;
+    height: 100%;
+`;
+
+element.innerHTML = `<iframe src="https://covid19japan.com/embed" style="${style}" />`;
