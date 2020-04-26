9d4a96a1d565dfc18b6b3af5ba5113afa6a54d3b
mertd <mertd@users.noreply.github.com>
2020-04-26 13:18:40 +0900

    add message listener

--- static/embed.js
+++ static/embed.js
@@ -2,6 +2,7 @@
  * Embed covid19japan.com into your web app
  */
 
+// configure embed
 const element = document.getElementById("covid19japan-embed");
 const style = `
     border: 0;
@@ -9,8 +10,25 @@ const style = `
     height: 100%;
 `;
 const src =
-  window.location.hostname === "localhost"
-    ? "http://localhost:4000/embed"
+  window.location.hostname === "" || window.location.hostname === "localhost"
+    ? "http://localhost:4000/"
     : "https://covid19japan.com/embed";
 
-element.innerHTML = `<iframe src="${src}" style="${style}" />`;
+// populate embed html
+element.innerHTML = `<iframe id="covid19japan-iframe" src="${src}" style="${style}" />`;
+
+// listen for resize
+window.addEventListener(
+  "message",
+  function (e) {
+    let frame = document.getElementById("covid19japan-iframe");
+    let message = e.data[0];
+    let data = e.data[1];
+    switch (message) {
+      case "setHeight":
+        frame.height(data);
+        break;
+    }
+  },
+  false
+);
