e464b92040474ec248887e120f60115d49671d04
Chris Loer <chris.loer@gmail.com>
2020-04-27 15:14:21 +0900

    Attempt to provide better context for case numbers: - Scale prefecture color-coding to population - Use extrusions to provide a linear indicator of case count

--- src/components/OutbreakMap/DrawMapPrefectures.js
+++ src/components/OutbreakMap/DrawMapPrefectures.js
@@ -2,6 +2,8 @@ import i18next from "i18next";
 import {
   PREFECTURE_JSON_PATH,
   PREFECTURE_PAINT,
+  PREFECTURE_POPULATIONS,
+  PREFECTURE_CENTERS,
   COLOR_YELLOW,
   COLOR_ORANGE,
   COLOR_RED,
@@ -31,26 +33,75 @@ const drawMapPrefectures = (pageDraws, ddb, map) => {
 
   // Start the Mapbox search expression
   const prefecturePaint = [...PREFECTURE_PAINT];
+
+  // Start building the GeoJSON source for extrusions
+  const prefectureCenters = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
   // Go through all prefectures looking for cases
   ddb.prefectures.map((prefecture) => {
     let cases = parseInt(prefecture.confirmed);
     if (cases > 0) {
+      const prefectureCenter = PREFECTURE_CENTERS[prefecture.name];
+      if (prefectureCenter) {
+        // Doesn't exist for Diamond Princess, etc.
+        // Make a rough-and-ready circle approximation for the base of the extrusion
+        const delta = 0.15;
+        const deltaCorner = delta / Math.sqrt(2);
+        prefectureCenters.features.push({
+          type: "Feature",
+          properties: { confirmed: cases }, // TODO: Does this need to be updated dynamically?
+          geometry: {
+            type: "Polygon",
+            coordinates: [
+              [
+                [prefectureCenter.long + delta, prefectureCenter.lat],
+                [
+                  prefectureCenter.long + deltaCorner,
+                  prefectureCenter.lat + deltaCorner,
+                ],
+                [prefectureCenter.long, prefectureCenter.lat + delta],
+                [
+                  prefectureCenter.long - deltaCorner,
+                  prefectureCenter.lat + deltaCorner,
+                ],
+                [prefectureCenter.long - delta, prefectureCenter.lat],
+                [
+                  prefectureCenter.long - deltaCorner,
+                  prefectureCenter.lat - deltaCorner,
+                ],
+                [prefectureCenter.long, prefectureCenter.lat - delta],
+                [
+                  prefectureCenter.long + deltaCorner,
+                  prefectureCenter.lat - deltaCorner,
+                ],
+                [prefectureCenter.long + delta, prefectureCenter.lat],
+              ],
+            ],
+          },
+        });
+      }
       prefecturePaint.push(prefecture.name);
 
-      if (cases <= 49) {
-        // 1-49 cases
+      let caseRate =
+        cases / (PREFECTURE_POPULATIONS[prefecture.name] / 1000000);
+
+      if (caseRate <= 49) {
+        // 1-49 cases/million
         prefecturePaint.push(COLOR_YELLOW);
-      } else if (cases <= 99) {
-        // 50-99 cases
+      } else if (caseRate <= 99) {
+        // 50-99 cases/million
         prefecturePaint.push(COLOR_ORANGE);
-      } else if (cases <= 499) {
-        // 100-499 cases
+      } else if (caseRate <= 149) {
+        // 100-149 cases/million
         prefecturePaint.push(COLOR_RED);
-      } else if (cases <= 999) {
-        // 500-999 cases
+      } else if (caseRate <= 199) {
+        // 150-199 cases/million
         prefecturePaint.push(COLOR_DARK_RED);
       } else {
-        // 1000+ cases
+        // 200+ cases/million
         prefecturePaint.push(COLOR_DARK_BURGUNDY);
       }
     }
@@ -65,6 +116,11 @@ const drawMapPrefectures = (pageDraws, ddb, map) => {
     map.addSource("prefectures", {
       type: "geojson",
       data: PREFECTURE_JSON_PATH,
+    });
+
+    map.addSource("prefecture-centers", {
+      type: "geojson",
+      data: prefectureCenters,
     });
 
     // Add the prefecture color layer to the map
@@ -94,6 +150,20 @@ const drawMapPrefectures = (pageDraws, ddb, map) => {
           "line-width": 0.5,
           "line-color": "#c0c0c0",
           "line-opacity": 0.5,
+        },
+      },
+      firstSymbolId
+    );
+
+    map.addLayer(
+      {
+        id: "prefecture-extrusion-layer",
+        type: "fill-extrusion",
+        source: "prefecture-centers",
+        paint: {
+          "fill-extrusion-color": "black",
+          "fill-extrusion-height": ["*", ["get", "confirmed"], 200],
+          "fill-extrusion-opacity": 0.7,
         },
       },
       firstSymbolId
@@ -136,6 +206,9 @@ const drawMapPrefectures = (pageDraws, ddb, map) => {
 
       const name = thisPrefecture.name;
       const confirmed = thisPrefecture.confirmed;
+      const casesPerMillion = Math.floor(
+        confirmed / (PREFECTURE_POPULATIONS[name] / 1000000)
+      );
       const deaths = thisPrefecture.deaths;
       const recovered = thisPrefecture.recovered;
       const active =
@@ -148,6 +221,9 @@ const drawMapPrefectures = (pageDraws, ddb, map) => {
           <span data-i18n="confirmed">${i18next.t(
             "confirmed"
           )}</span>: ${confirmed} ${popupIncrementSpan}<br />
+          <span data-i18n="Cases per million">${i18next.t(
+            "Cases per million"
+          )}</span>: ${casesPerMillion} ${popupIncrementSpan}<br />
           <span data-i18n="recovered">${i18next.t(
             "recovered"
           )}</span>: ${recovered}<br />
