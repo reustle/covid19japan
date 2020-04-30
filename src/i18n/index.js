b6dd4b3037ef64107eeade7031ef2d6d031f6bf8
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-30 17:12:07 +0200

    Add French in languages and flags array.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,6 @@
 // Add new languages and their emoji flag here. Make sure the array indices line up.
-export const LANGUAGES = ["en", "ja"];
-export const FLAGS = ["🇺🇸", "🇯🇵"];
+export const LANGUAGES = ["en", "ja", "fr"];
+export const FLAGS = ["🇺🇸", "🇯🇵", "🇫🇷"];
 
 const generateExport = () => {
   const resources = {};
