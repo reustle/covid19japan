cb7f056e05e2d353e74b819e39c8f96dfbdd54db
Alastair Tse <alastair@liquidx.net>
2020-07-16 00:47:57 +0900

    Dynamically generate the region table.

--- src/components/RegionalCharts/RegionalCharts.js
+++ src/components/RegionalCharts/RegionalCharts.js
@@ -1,3 +1,6 @@
+import { el, div } from "../quickelement";
+import _ from "lodash";
+
 const drawRegionChart = (chartName, element) => {
   let svgURL = `https://data.covid19japan.com/charts/${chartName}`;
   fetch(svgURL)
@@ -12,6 +15,59 @@ const drawRegionChart = (chartName, element) => {
         element.innerHTML = text;
       }
     });
+};
+
+export const createRegionBox = (regionName, region) => {
+  const regionId = regionName.toLowerCase();
+  const box = div(["region-box", "region-area"], { id: "region-osaka" }, [
+    div("vitals", {}, [
+      div("label", { "data-i18n": `regions.${regionId}` }, regionName),
+      div(["active", "metric"], {}, [
+        div("value-label", { "data-i18n": "active" }, "Active"),
+        div("value", {}, "" + region.active),
+        div("diff", null, "&nbsp;"),
+      ]),
+      div(["confirmed", "metric"], {}, [
+        div("value-label", { "data-i18n": "confirmed" }, "Confirmed"),
+        div("value", {}, "" + region.confirmed),
+        div("diff", null, `&nbsp;(+${region.newlyConfirmed})`),
+      ]),
+      div(["deceased", "metric"], {}, [
+        div("value-label", { "data-i18n": "deceased" }, "Deceased"),
+        div("value", {}, "" + region.deceased),
+        div("diff", null, "&nbsp;"),
+      ]),
+    ]),
+    div("region-box-prefectures"),
+  ]);
+  return box;
+};
+
+export const createPrefectureBox = (prefecture) => {
+  const prefectureId = prefecture.name.toLowerCase();
+  const box = div(
+    ["region-box", "region-inner-box"],
+    { id: `region-${prefectureId}` },
+    [
+      div(
+        "label",
+        { "data-i18n": `prefectures.${prefectureId}` },
+        prefecture.name
+      ),
+      div(["confirmed", "metric"], {}, [
+        div("value-label", { "data-i18n": "confirmed" }, "Confirmed"),
+        div("value", {}, "" + prefecture.confirmed),
+        div("diff", null, `&nbsp;(+${prefecture.newlyConfirmed})`),
+      ]),
+      div("chart"),
+      div(
+        "chart-caption",
+        { "data-i18n": "regional-chart-caption" },
+        "Confirmed cases by day"
+      ),
+    ]
+  );
+  return box;
 };
 
 export const drawRegionalCharts = (prefectureData, regionalData) => {
@@ -33,28 +89,46 @@ export const drawRegionalCharts = (prefectureData, regionalData) => {
     }
   }
 
+  const regionPrefectures = {
+    Kanto: ["Tokyo", "Saitama", "Kanagawa", "Chiba"],
+    Kansai: ["Osaka", "Hyogo", "Kyoto", "Nara"],
+    Kyushu: ["Fukuoka", "Kagoshima", "Saga", "Nagasaki"],
+    Chubu: ["Aichi", "Gifu", "Shizuoka", "Niigata"],
+    Tohoku: ["Miyagi", "Fukushima", "Aomori", "Akita"],
+    Shikoku: ["Kagawa", "Kochi", "Ehime", "Tokushima"],
+    Chugoku: ["Hiroshima", "Okayama", "Shimane", "Yamaguchi"],
+    Hokkaido: ["Hokkaido"],
+  };
+
   if (regionalData) {
-    for (let regionName of Object.keys(regionalData)) {
-      let regionId = regionName.toLowerCase();
-      let regionBox = document.querySelector(`#region-${regionId}`);
-      if (regionBox) {
-        let regionData = regionalData[regionName];
-        let e = null;
-        e = regionBox.querySelector(".active .value");
-        if (e) {
-          e.innerHTML = `${regionData.active}`;
-        }
-        e = regionBox.querySelector(".confirmed .value");
-        if (e) {
-          e.innerHTML = `${regionData.confirmed}`;
-        }
-        e = regionBox.querySelector(".confirmed .diff");
-        if (e) {
-          e.innerHTML = `(+${regionData.newlyConfirmed})`;
-        }
-        e = regionBox.querySelector(".deceased .value");
-        if (e) {
-          e.innerHTML = `${regionData.deceased}`;
+    const regionalContainer = document.querySelector(
+      "#regional-charts-container"
+    );
+    const sortedRegions = Object.entries(regionalData).sort((a, b) => {
+      return parseInt(b[1].active) - parseInt(a[1].active);
+    });
+    for (let [regionName, regionData] of sortedRegions) {
+      let regionBox = createRegionBox(regionName, regionData);
+      regionalContainer.appendChild(regionBox);
+
+      let prefectureNames = regionPrefectures[regionName];
+      if (prefectureNames) {
+        let prefecturesContainer = regionBox.querySelector(
+          ".region-box-prefectures"
+        );
+        for (let prefectureName of prefectureNames) {
+          for (let prefecture of prefectureData) {
+            if (prefecture.name == prefectureName) {
+              let prefectureBox = createPrefectureBox(prefecture);
+              let chartElement = prefectureBox.querySelector(".chart");
+              let prefectureId = prefecture.name.toLowerCase();
+              drawRegionChart(
+                `${prefectureId}_confirmed_daily.svg`,
+                chartElement
+              );
+              prefecturesContainer.appendChild(prefectureBox);
+            }
+          }
         }
       }
     }
---
+++ src/components/quickelement/index.js
@@ -0,0 +1,41 @@
+export const el = (tag, klass, attributes, contents) => {
+  const element = document.createElement(tag);
+  if (attributes && attributes.className) {
+    element.classList.add(attributes.className);
+  }
+  if (klass && typeof klass == "string") {
+    element.classList.add(klass);
+  }
+  if (klass && typeof klass == "object" && klass.length > 0) {
+    element.classList.add(...klass);
+  }
+  if (attributes) {
+    for (let attrKey of Object.keys(attributes)) {
+      if (attrKey == "className") {
+        continue;
+      }
+      element.setAttribute(attrKey, attributes[attrKey]);
+    }
+  }
+  if (typeof contents == "string") {
+    let textNode = document.createElement("span");
+    textNode.innerHTML = contents;
+    element.appendChild(textNode);
+  } else if (typeof contents == "object" && contents.length) {
+    for (let child of contents) {
+      if (child) {
+        element.appendChild(child);
+      }
+    }
+  }
+  return element;
+};
+
+export const td = (klass, attributes, contents) =>
+  el("td", klass, attributes, contents);
+export const tr = (klass, attributes, contents) =>
+  el("tr", klass, attributes, contents);
+export const div = (klass, attributes, contents) =>
+  el("div", klass, attributes, contents);
+export const img = (klass, attributes, contents) =>
+  el("img", klass, attributes, contents);
