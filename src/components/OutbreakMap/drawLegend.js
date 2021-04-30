61d02381a1cdc9ef4951e7d6ad4b2112db7833f9
AndriiAndrey <58549103+AndriiAndrey@users.noreply.github.com>
2021-04-30 13:21:19 +0300

    added static map on load and dymamic map will replace it on click (#492)
    * added static map on load and dymamic map will replace it on click, currently for static map used my own styles need to replace
    
    * added custom styles for static map and put it to work with production mapbox account
    
    * replaced var with let, updated KPI links, added map lenend redraw on language change
    
    * refactored getPrefecturePaint helper function to accept only prefectures data instead of all data

---
+++ src/components/OutbreakMap/drawLegend.js
@@ -0,0 +1,33 @@
+import i18next from "i18next";
+import { LEGEND_CLASSES, MAP_COLOR_BOUNDARIES } from "../../data/constants";
+import { maybeIntlNumberFormat } from "../../i18n";
+
+const getLegendLabel = (boundary, previousBoundary, formatNumber) => {
+  if (previousBoundary === 0) {
+    return i18next.t("cases-none");
+  }
+  if (!isFinite(boundary)) {
+    return i18next.t("cases-last", { from: formatNumber(previousBoundary) });
+  }
+  return i18next.t("cases-range", {
+    from: formatNumber(previousBoundary),
+    to: formatNumber(boundary - 1),
+  });
+};
+
+export const drawLegend = (lang) => {
+  const formatNumber = maybeIntlNumberFormat(lang);
+  let classIndex = 0;
+  let previousBoundary = 0;
+  let html = "";
+  for (let boundary of Object.keys(MAP_COLOR_BOUNDARIES).sort(
+    (a, b) => a - b
+  )) {
+    let label = getLegendLabel(boundary, previousBoundary, formatNumber);
+    html += `<div><span class="${LEGEND_CLASSES[classIndex]}">▉</span> ${label}</div>`;
+
+    classIndex = (classIndex + 1) % LEGEND_CLASSES.length;
+    previousBoundary = boundary;
+  }
+  return html;
+};
